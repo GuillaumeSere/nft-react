4152f5b74a2c268398367128b6502ea8769c3f57
Guillaume Sere <guillaumesere@hotmail.fr>
2022-10-14 14:07:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Getting Started with Create React App
+
+
+![20221014133727](https://user-images.githubusercontent.com/75996200/195838090-c8a16ca1-a10c-4035-a6aa-fb464479c454.png)
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import HeroSection from '../components/ui/HeroSection'
+import LiveAction from '../components/ui/Live-action/LiveAction'
+import SellerSection from '../components/ui/Seller-section/SellerSection'
+import StepSection from '../components/ui/Step-section/StepSection'
+import Trending from '../components/ui/Trending-section/Trending'
+
+const Home = () => {
+  return (
+    <>
+    <HeroSection />
+    <LiveAction />
+    <SellerSection />
+    <Trending />
+    <StepSection />
+    </>
+  )
+}
+
+export default Home
